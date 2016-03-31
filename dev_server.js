d200514ce278a426d85a168a97ef1e75662b1ec8
MirekSz <miro1994>
2016-03-31 19:14:59 +0200

    init

---
+++ dev_server.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.dev.js');
+
+var app = express();
+var compiler = webpack(config);
+var port = process.env.PORT || 3001;
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    quiet: false,
+    reload: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, req.originalUrl));
+});
+
+
+app.listen(port, 'localhost', err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(`Listening at http://localhost:${port}`);
+});
---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var APP_DIR = path.join(__dirname, '..', 'app');
+var TESTS_DIR = path.join(__dirname, '..', 'tests');
+
+var minified_libs = path.join(__dirname, '..', 'lib');
+
+var deps = [
+    'lodash/lodash.min.js'
+];
+
+
+
+module.exports = {
+    devtool: 'cheap-module-source-map',
+    entry: {
+       app:[ 'webpack-hot-middleware/client?http://localhost:3000&overlay=true',
+            './src/index.js'],
+        test:[ 'babel-polyfill','webpack-hot-middleware/client?http://localhost:3000&overlay=true&reload=true',
+            './autoDiscovery.js'],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.CommonsChunkPlugin('common.js')
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['react-hot', 'babel?cacheDirectory'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
+};
